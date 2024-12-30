11386c663e5b0ca50b7eaeeea8b97217e480e651
martinjolif <martinjolif@gmail.com>
2024-12-30 11:39:07 +0100

    enable gzip for the checkpoints

--- src/train.py
+++ src/train.py
@@ -3,6 +3,8 @@ from gymnasium.wrappers import TimeLimit
 from env_hiv import HIVPatient
 from utils import greedy_action_FQI
 
+import gzip
+
 env = TimeLimit(
     env=HIVPatient(domain_randomization=False), max_episode_steps=200
 )  # The time wrapper limits the number of steps in an episode at 200.
@@ -14,12 +16,12 @@ env = TimeLimit(
 # ENJOY!
 class ProjectAgent:
     def act(self, observation, use_random=False):
-        action = greedy_action_FQI(self.QfunctionsLoaded[-1], observation, env.action_space.n)
+        action = greedy_action_FQI(self.QfunctionsLoaded, observation, env.action_space.n)
         return action
 
     def save(self, path):
         pass
 
     def load(self):
-        with open('FQI_Q_functions.pkl', 'rb') as f:
+        with gzip.open('FQI_Q_functions.pkl.gz', 'rb') as f:
             self.QfunctionsLoaded = pickle.load(f)
